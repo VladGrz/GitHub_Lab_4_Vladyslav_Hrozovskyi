709818419f8eae30b20f251c2ee493d72db4f993
VladGrz <60891301+VladGrz@users.noreply.github.com>
2020-05-09 11:38:23 +0300

    Update UnitTest1.cs

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -11,10 +11,12 @@ namespace UnitTestProject1
         {
             Student s = new Student();
             string result = Lab_4_zavd_1.Student.StudentRating(s.rating);
-            Assert.AreEqual("Можна вчитися краще!",result);
+            string str = "Можна вчитися краще!";
+            Assert.AreEqual(str, result);
             s.rating = 65;
             result = Lab_4_zavd_1.Student.StudentRating(s.rating);
-            Assert.AreEqual("Варто бiльше уваги придiляти навчанню!", result);
+            string stroka = "Варто бiльше уваги придiляти навчанню!";
+            Assert.AreEqual(stroka, result);
         }
     }
 }
